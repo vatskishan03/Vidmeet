4f6f51a4839767c9e14e1231459b1a60a54d5da7
Kishan <kishanvats2003@gmail.com>
2024-12-22 12:07:41 +0530

    Done

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -25,12 +25,6 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
-            <Image
-              src="/icons/logo.svg"
-              width={32}
-              height={32}
-              alt="yoom logo"
-            />
             <p className="text-[26px] font-extrabold text-white">Vidmeet</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
