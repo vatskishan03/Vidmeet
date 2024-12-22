e06e6ca63f6e24ac8cd98c3e90161410661d984f
Kishan <kishanvats2003@gmail.com>
2024-12-22 12:12:28 +0530

    Done

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -25,6 +25,7 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-dark-1">
           <Link href="/" className="flex items-center gap-1">
+
             <p className="text-[26px] font-extrabold text-white">Vidmeet</p>
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
